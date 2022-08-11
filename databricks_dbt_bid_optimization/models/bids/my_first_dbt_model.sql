dec6b8c0df346e43da4d3b7a4405e3eb8d27d32f
mojgan.mazouchi@databricks.com <mojgan.mazouchi@databricks.com>
2022-08-11 02:26:42 +0000

    profile name

--- databricks_dbt_bid_optimization/models/bids/my_first_dbt_model.sql
+++ databricks_dbt_bid_optimization/models/bids/my_first_dbt_model.sql
@@ -9,16 +9,9 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
 
 select *
-from source_data
+from rbt_2
 
 /*
     Uncomment the line below to remove records with null `id` values
